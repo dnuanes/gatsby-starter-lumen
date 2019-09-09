830e4feff4d07ec2c8566a79de0de90e327020b7
dnuanes <dnuanes@tulane.edu>
2019-09-09 15:37:23 -0500

    added mini headshot

--- src/components/Sidebar/Author/Author.test.js
+++ src/components/Sidebar/Author/Author.test.js
@@ -7,7 +7,7 @@ describe('Author', () => {
   const props = {
     author: {
       name: 'test',
-      photo: '/photo.jpg',
+      photo: '/headshot.jpg',
       bio: 'test'
     },
     isIndex: false
